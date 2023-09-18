e935a8e88d5760bb263d5cb8a3d958b256326c94
Plutonchi <boburbilol2016@gmail.com>
2023-09-18 16:05:12 +0600

    search is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:magazine/page/page.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
